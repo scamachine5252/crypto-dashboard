4e5d72fbe6f88ada8998d4cc1a89b2cde5e9fa26
Хусяинов Рафаэль Равилевич <rafa_99@MacBook-Pro-Husainov.local>
2026-05-11 22:55:34 +0300

    fix: load .env.local in worker via dotenv

--- worker/index.ts
+++ worker/index.ts
@@ -1,4 +1,5 @@
-import 'dotenv/config'
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env.local' })
 import { ConnectorManager } from './connector-manager'
 import { startBalancePoller } from './balance-poller'
 
