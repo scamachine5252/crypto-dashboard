466be179b7b39b9b0e98435f38b471ef65f58128
Хусяинов Рафаэль Равилевич <rafa_99@MacBook-Pro-Husainov.local>
2026-05-12 19:54:59 +0300

    feat: start FullHistorySyncer in worker process alongside ConnectorManager

--- worker/index.ts
+++ worker/index.ts
@@ -1,21 +1,28 @@
 import dotenv from 'dotenv'
 dotenv.config({ path: '.env.local' })
 import { ConnectorManager } from './connector-manager'
+import { FullHistorySyncer } from './full-history-syncer'
 import { startBalancePoller } from './balance-poller'
 
 async function main() {
   console.log('[worker] starting...')
 
-  const manager = new ConnectorManager(process.env.REDIS_URL ?? 'redis://127.0.0.1:6379')
+  const redisUrl = process.env.REDIS_URL ?? 'redis://127.0.0.1:6379'
+
+  const manager = new ConnectorManager(redisUrl)
+  const syncer  = new FullHistorySyncer(redisUrl)
 
   startBalancePoller()
 
   await manager.start()
-  console.log('[worker] all connectors started')
+  await syncer.start()
+
+  console.log('[worker] all connectors + full-history syncer started')
 
   const shutdown = () => {
     console.log('[worker] shutting down...')
     manager.stop()
+    syncer.stop()
     process.exit(0)
   }
 
